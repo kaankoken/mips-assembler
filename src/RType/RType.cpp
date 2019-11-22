7334b25e55395a152d1e19d459fc50ef7e6060a1
kaankoken <e215206@metu.edu.tr>
2019-11-23 00:23:07 +0200

    Comments are added

--- src/RType/RType.cpp
+++ src/RType/RType.cpp
@@ -1,19 +1,33 @@
 #include "RType.h"
 #include <iostream>
 
+/**
+ * Constructor of Rtype instructions
+ * It also used for populating the data
+*/
 RType::RType() {
     rTypeInst.insert({"test", {5, 5, 5}});
     rTypeInst.insert({"test1", {6, 5, 5}});
 }
-
+/**
+ * Descrutive of Rtype instructions
+*/
 RType::~RType() {
     rTypeInst.clear();
 }
-
+/**
+ * @return unordered_map<string, tuple<int, int, int>>
+ * It returns the whole instruction set that defined.
+*/
 std::unordered_map<std::string, std::tuple<int, int, int>> RType::getWholeInst() const {
     return rTypeInst;
 }
-
+/**
+ * @param key -> string
+ * @return tuple<int, int, int>
+ * if instruction is found return tuple
+ * else return -1, -1 ,-1
+*/
 std::tuple<int, int, int> RType::findInst(std::string key) const {
     auto instruction = rTypeInst.find(key);
     
