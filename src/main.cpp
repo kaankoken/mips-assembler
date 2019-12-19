09a23de05e6b78a2a51d64265f12672f96dca1b9
kaankoken <e215206@metu.edu.tr>
2019-12-19 02:53:30 +0200

    integration completed. To main class, displayInstruction method added. Infinite loop error fixed while writing into file.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,10 @@
 #include <fstream>
 #include <filesystem>
 #include <vector>
+#include <map>
 
+#include "SyntaxChecker.h"
+#include "BitConverter.h"
 class Main : ErrorHandling {
     public:
         Main() { ErrorHandling(); }
@@ -28,31 +31,34 @@ class Main : ErrorHandling {
             return menuOption.at(0);
         }
 
-        void readFile(std::string fileName) {
+        std::vector<std::string> readFile(std::string fileName) const {
             bool isExist = std::filesystem::exists(fileName);
+            std::vector<std::string> instructionSet;
+
             if (isExist) {
                 std::fstream file(fileName);
                 if (file.is_open()) {
                     std::string line;
-                    while (getline(file, line)) {
-                        std::cout << line << std::endl;
-                        // get the new codes from file and process it
-                    }
+                    while (getline(file, line))
+                        instructionSet.push_back(line);
                     file.close();
                 }
             }
+            else
+                std::cout << "File does not Exist!!!" << std::endl;
+            return instructionSet;
         }
 
         void writeFile(std::vector<std::string> instructionSet){
-            std::ofstream outfile ("output.txt");
+            std::ofstream outfile ("output.obj");
             std::vector<std::string>::iterator it = instructionSet.begin();
 
             while(it != instructionSet.end()){
                 outfile << *it << std::endl;
+                it++;
             }
 
             outfile.close();
-
         }
 
         std::pair<std::map<std::string, int>, std::vector<int>> labelAndPcCounter(std::vector<std::string> instructionSet) {
@@ -75,29 +81,96 @@ class Main : ErrorHandling {
             
             return std::make_pair(labels, pcCounter);;
         }
+
+        void displayInstructions(std::vector<std::string> instructionSet) {
+            std::vector<std::string>::iterator it = instructionSet.begin();
+
+            while(it != instructionSet.end()) {
+                std::cout << *it << std::endl;
+                it++;
+            }
+        }
 };
 
 int main(int argc, char **argv) {
-    Main assembler;
+    std::vector<std:: string> instructionSet;
     char menuOption;
     
     std::string fileName = "./test1.txt";
-    assembler.readFile(fileName);
     std::cout << "Welcome to the MIPS ASSEMBLER" << std::endl;
-
+    std::cout << "Made By Kaan Taha Köken & Ege Seçkin" << std::endl << std::endl;;
+    
+    Main assembler;
+    SyntaxChecker syntax;
+    BitConverter converter;
+    //assembler.readFile(fileName);
+    std::pair<std::map<std::string, int>, std::vector<int>> labelsAndPc;
+    std::vector<std::vector<std::string>> instructions;
+    std::vector<std::string> result;
     do {
         menuOption = assembler.menu();
         switch (menuOption) {
-            case '1':
+            case '1': {
+                int numbOfInst;
+                std::cout << "\nHow many lines of code will be entered" << std::endl;
+                std::cout << "Answer: ";
+                std::cin >> numbOfInst;
+                std::string temp;
+                while(numbOfInst > 0 && std::getline(std::cin, temp)) {
+                    if (temp.compare("")) {
+                        instructionSet.push_back(temp);
+                        numbOfInst--;
+                    }
+                }
                 break;
+            }
             case '2':
+                std::cout << "\nEnter a file name: ";
+                std::cin >> fileName;
+                instructionSet = assembler.readFile(fileName);
                 break;
             case '3':
+                std::cout << "\nGood Bye!!!" << std::endl;
                 break;
             default:
                 std::cout << "\nWrong Option!!!" << std::endl;
                 break;
         }
+
+        labelsAndPc = assembler.labelAndPcCounter(instructionSet);
+        std::vector<std::string>::iterator it = instructionSet.begin();
+        int size = 0;
+        while (size < instructionSet.size() && it != instructionSet.end()) {
+            instructions.push_back(syntax.checkSyntax(*it));
+            size++;
+            it++;
+        }
+
+        std::vector<std::vector<std::string>>::iterator instIterator = instructions.begin();
+        std::vector<int>::iterator pcIterator = labelsAndPc.second.begin();
+       
+        while (instIterator != instructions.end()) {
+            std::string type;
+            if (instIterator->at(0)[instIterator->at(0).size() - 1] == ':')
+                type = instIterator->at(1);
+            else
+                type = instIterator->at(0);
+
+            result.push_back(converter.DecToBin(syntax.checkInstType(type), *instIterator, labelsAndPc.first, *pcIterator));
+            instIterator++;
+            pcIterator++;
+        }
+
+        if (menuOption == '1')
+            assembler.displayInstructions(result);
+        else if (menuOption == '2')
+            assembler.writeFile(result);
+        
+        result.clear();
+        labelsAndPc.first.clear();
+        labelsAndPc.second.clear();
+        instructions.clear();
+        instructionSet.clear();
     } while (menuOption != '3');
     
     return 0;
